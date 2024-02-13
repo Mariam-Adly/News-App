f02344a0e6142eb6c3701d0bd8c81af8169c8c60
Mariam Adly <mariamadly712@gmail.com>
2024-02-13 15:12:18 -0800

    Initial Commit

---
+++ News App/News_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  News_AppApp.swift
+//  News App
+//
+//  Created by mariam adly on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
